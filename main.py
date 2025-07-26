ab2e8ec80edd1822f6dea18186718bc41fc5b84e
CaoXin <caoxin@xysu.tech>
2025-07-26 22:22:35 +0800

    feat: basic function

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
+import pygame
+import time
+import serial
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+pygame.joystick.init()
 
+joystick_count = pygame.joystick.get_count()
+if joystick_count == 0:
+    print("Unable to find any controller！")
+    exit()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
 
+print(f"Connect to controller: {joystick.get_name()}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+ser = serial.Serial('COM11', 115200, timeout=1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+control_val = {
+    "A": 0,
+    "B": 0,
+    "X": 0,
+    "Y": 0,
+    "LEFT_SHOULDER": 0,
+    "RIGHT_SHOULDER": 0,
+    "LEFT_THUMB": 0,
+    "RIGHT_THUMB": 0,
+    "LEFT_TRIGGER": 0.0,
+    "RIGHT_TRIGGER": 0.0,
+    "LEFT_JOYSTICK_X": 0.0,
+    "LEFT_JOYSTICK_Y": 0.0,
+    "RIGHT_JOYSTICK_X": 0.0,
+    "RIGHT_JOYSTICK_Y": 0.0
+}
+
+handle_map = {
+    0: "LEFT_JOYSTICK_X",
+    1: "LEFT_JOYSTICK_Y",
+    2: "RIGHT_JOYSTICK_X",
+    3: "RIGHT_JOYSTICK_Y",
+    4: "RIGHT_TRIGGER",
+    5: "LEFT_TRIGGER"
+}
+
+# main loop
+try:
+    while True:
+        # Handle Controller Event
+        for event in pygame.event.get():
+            # Trigger and Joystick event
+            if event.type == pygame.JOYAXISMOTION:
+                for axis in range(joystick.get_numaxes()):
+                    axis_value = joystick.get_axis(axis)
+                    # print(f"Handle {axis} value: {axis_value:.2f}")
+                    if axis in handle_map:
+                        control_val[handle_map[axis]] = axis_value
+
+        # print(f"x_speed: {control_val['LEFT_JOYSTICK_Y']}, y_speed：{control_val['RIGHT_JOYSTICK_X']}")
+
+        command = "CONTROL " + str(int(control_val['RIGHT_JOYSTICK_X'] * 10)) + ' ' + str(int(control_val['LEFT_JOYSTICK_Y'] * 10))
+        ser.write((command + "\n").encode())
+        print("Tx: " + command)
+
+        # if ser.in_waiting:
+        #     line = ser.readline().decode(errors='ignore').strip()
+        #     if line:
+        #         print("Rx：", line)
+
+        time.sleep(0.01)
+
+except KeyboardInterrupt:
+    print("Keyboard interrupt")
+finally:
+    pygame.quit()
